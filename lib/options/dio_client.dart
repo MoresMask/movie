8f1ac2fcd59762944ccfdaf762c805ff92be1e42
MoresMask <byasov.fd@gmail.com>
2022-12-10 11:16:28 +0300

    first

--- lib/options/dio_client.dart
+++ lib/options/dio_client.dart
@@ -2,17 +2,18 @@ import 'package:dio/dio.dart';
 
 abstract class DioClient {
   static Future<Dio> createDIO({
-    required Future<List<Interceptor>> Function(Dio) getInterceptors,
     required String baseUrl,
   }) async {
     var _dio = Dio();
 
     _dio
+      ..options.headers = {
+        'X-RapidAPI-Key': 'aa5cbf915emshf46dbe98ec2f279p1e920ajsn8ce46f9cd650'
+      }
       ..options.baseUrl = baseUrl
       ..options.connectTimeout = Duration.millisecondsPerMinute - 45000
       ..options.receiveTimeout = Duration.millisecondsPerMinute - 45000
-      ..httpClientAdapter
-      ..interceptors.addAll(await getInterceptors(_dio));
+      ..httpClientAdapter;
 
     return _dio;
   }
